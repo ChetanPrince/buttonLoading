bfd341174278f9f43bf828c09b7d429f214a77d2
ChetanPrince <saini.rajvir2@gmail.com>
2024-07-17 22:13:07 +0200

    spin button style adjusted and now script has to perform its due dilligence

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ passwordView.addEventListener("click", ()=>{
             password.type = "password";
         }
 })
+
+
+
+
+// everything inside a click event and after checking whether username has an eligible email id 
+
+//  check whether the password meets requirement of having first letter case capital and minimum 8 characters with 1 special character
+
+// add btn-text class to submit button
+// add span element and set id to spin
+//  add class spin animation when click event happens
+// set a timeout 
+//  after a timeout remove element span with id spin
+// after a timeout remove btn text class from submit button
